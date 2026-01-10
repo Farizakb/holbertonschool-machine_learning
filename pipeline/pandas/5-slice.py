f8f17833e8ef97faf7ec27fda6aa2d7845decb47
farizakb <farizakbarzada@gmail.com>
2026-01-10 14:10:01 +0400

    commit 5

---
+++ pipeline/pandas/5-slice.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module to slice specific data from a pandas DataFrame
+"""
+
+def slice(df):
+    """
+    Takes a pd.DataFrame and returns the row
+    with index label 60 and the columns High, Low, Close, and Volume_BTC
+    Args:
+        df: pd.DataFrame containing the relevant columns
+    Returns:
+        pd.DataFrame sliced to the specified row and columns
+    """
+   
+    sliced_df = df.loc[60, ['High', 'Low', 'Close', 'Volume_BTC']]
+    return sliced_df
