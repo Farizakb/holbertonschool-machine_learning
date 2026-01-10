ee6c1438e6d03644465bf53c0879d2902ea6b312
farizakb <farizakbarzada@gmail.com>
2026-01-10 13:34:12 +0400

    commit 2

---
+++ pipeline/pandas/2-from_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Module to create a pandas DataFrame from a CSV file
+"""
+import pandas as pd
+
+def from_file(filename, delimiter):
+    """
+    Creates a pd.DataFrame from a CSV file
+
+    Args:
+        filename: The path to the CSV file
+        delimiter: The delimiter used in the CSV file
+    Returns:
+        The newly created pd.DataFrame
+    """
+    # Read the CSV file into a DataFrame using the specified delimiter
+    df = pd.read_csv(filename, delimiter=delimiter)
+    return df
