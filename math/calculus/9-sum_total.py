a60c16edcb4dc6a3c028825c276cc561627a7eab
farizakb <farizakbarzada@gmail.com>
2026-01-16 18:42:08 +0400

    commit 9

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,17 @@
+# !/usr/bin/env python3
+"""Module for calculating the summation of i squared from 1 to n."""
+
+
+
+def summation_i_squared(n):
+    """Calculates the summation of i squared from 1 to n.
+
+    Args:
+        n (int): The upper limit of the summation.
+    Returns:
+        int: The summation of i squared from 1 to n.
+    """
+    if type(n) is not int or n < 1:
+        return None
+    result = (n * (n + 1) * (2 * n + 1)) / 6
+    return int(result)
