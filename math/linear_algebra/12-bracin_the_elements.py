13e18dfb50cf38e44093e942f7a41253d8f26757
farizakb <farizakbarzada@gmail.com>
2026-01-24 17:38:22 +0400

    commit 12.1

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -2,5 +2,6 @@
 
 
 def np_elementwise(mat1, mat2):
-    """Calculates the element-wise sum, difference, product, and quotient of two numpy.ndarray objects"""
+    """Calculates the element-wise sum, difference, product,
+    and quotient of two numpy.ndarray objects"""
     return mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2
