dcc29789334ff74e21aa97128805491f5da87dd6
farizakb <farizakbarzada@gmail.com>
2026-01-24 15:55:33 +0400

    commit 4.1

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
-""" A script that adds two arrays element-wise"""
+""" A script that adds two arrays element-wise """
+
 
 def add_arrays(arr1, arr2):
-    new_list = []
+    """ A function that adds two arrays element-wise """
+    if len(arr1) != len(arr2):
+        return None
+
+    result = []
     for i in range(len(arr1)):
-        new_list.append(arr1[i] + arr2[i])
-    return new_list
+        result.append(arr1[i] + arr2[i])
+
+    return result
