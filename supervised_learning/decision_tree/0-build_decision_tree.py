f03fc7fba7877397639a231c560000d7144f6f72
farizakb <farizakbarzada@gmail.com>
2026-02-21 17:24:55 +0400

    fix some mistkaes commit 0.2

--- supervised_learning/decision_tree/0-build_decision_tree.py
+++ supervised_learning/decision_tree/0-build_decision_tree.py
@@ -2,8 +2,10 @@
 
 import numpy as np
 
+
 class Node:
-    def __init__(self, feature=None, threshold=None, left_child=None, right_child=None, is_root=False, depth=0):
+    def __init__(self, feature=None, threshold=None, left_child=None,
+                 right_child=None, is_root=False, depth=0):
         self.feature = feature
         self.threshold = threshold
         self.left_child = left_child
@@ -14,10 +16,9 @@ class Node:
         self.depth = depth
 
     def max_depth_below(self):
-        if self.is_leaf:
-            return self.depth
-        else:
-            return max(self.left_child.max_depth_below(), self.right_child.max_depth_below())
+        return max(self.left_child.max_depth_below(),
+                   self.right_child.max_depth_below())
+
 
 class Leaf(Node):
     def __init__(self, value, depth=None):
@@ -29,8 +30,10 @@ class Leaf(Node):
     def max_depth_below(self):
         return self.depth
 
+
 class Decision_Tree():
-    def __init__(self, max_depth=10, min_pop=1, seed=0, split_criterion="random", root=None):
+    def __init__(self, max_depth=10, min_pop=1, seed=0,
+                 split_criterion="random", root=None):
         self.rng = np.random.default_rng(seed)
         if root:
             self.root = root
