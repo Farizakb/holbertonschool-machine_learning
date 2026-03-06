d0c1301b7f37f783b6b3a570ef7b79d24fa91bc6
farizakb <farizakbarzada@gmail.com>
2026-03-06 19:29:43 +0400

    commit 3

---
+++ supervised_learning/classification/3-neuron.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+"""
+    Class Neuron
+"""
+
+import numpy as np
+
+
+class Neuron:
+    """
+        Class Neuron : define single neuron performing binary classification
+    """
+
+    def __init__(self, nx):
+        """
+        class constructor
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """
+            The weights vector for the neuron
+
+            :return: value for private attribute __W
+        """
+        return self.__W
+
+    @property
+    def b(self):
+        """
+            The bias for the neuron
+
+            :return: value for private attribute __b
+        """
+        return self.__b
+
+    @property
+    def A(self):
+        """
+            The activated output of the neuron (prediction)
+
+            :return: value for private attribute __A
+        """
+        return self.__A
+
+    def forward_prop(self, X):
+        """
+        Performs the forward propagation step for the neuron
+
+        :param X: Input data (numpy array of shape (nx, m))
+        :return: Activated output of the neuron (shape (1, m))
+        """
+        Z = np.dot(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A
+    
+    def cost(self, Y, A):
+        """
+        Calculates the cost of the neuron
+
+        :param Y: True labels (numpy array of shape (1, m))
+        :param A: Activated output (numpy array of shape (1, m))
+        :return: Cost (float)
+        """
+        m = Y.shape[1]
+        cost = -1/m * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        return cost
