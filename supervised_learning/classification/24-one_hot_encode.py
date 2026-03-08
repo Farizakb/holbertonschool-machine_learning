8e55cb56f9d4d259ee77b3f68c234b406f980813
farizakb <farizakbarzada@gmail.com>
2026-03-08 18:11:50 +0400

    commit 24 25

---
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+    One-Hot Encode
+"""
+
+
+import numpy as np
+
+def one_hot_encode(Y, classes):
+    """
+    Converts a numerical label vector into a one-hot vector
+    """
+    if not isinstance(Y, np.ndarray) or len(Y.shape) != 1:
+        return None
+    if not isinstance(classes, int) or classes < np.max(Y) + 1:
+        return None
+
+    encoded_array = np.zeros((classes, Y.size), dtype=float)
+    encoded_array[Y, np.arange(Y.size)] = 1
+
+    return encoded_array
