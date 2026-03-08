b66086cabf98900a5f659ff3ee0ac24b406646fc
farizakb <farizakbarzada@gmail.com>
2026-03-08 16:16:43 +0400

    commit 18

---
+++ supervised_learning/classification/18-deep_neural_network.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+"""
+DeepNeuralNetwork performing binary classification
+"""
+
+
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """
+    Class that represents a deep neural network for binary classification
+    """
+
+    def __init__(self, nx, layers):
+        """
+        Class constructor
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(layers) is not list or len(layers) < 1:
+            raise TypeError("layers must be a list of positive integers")
+
+        weights = {}
+        previous = nx
+
+        for index, layer in enumerate(layers, 1):
+
+            if type(layer) is not int or layer < 0:
+                raise TypeError("layers must be a list of positive integers")
+
+            weights["b{}".format(index)] = np.zeros((layer, 1))
+            weights["W{}".format(index)] = (
+                np.random.randn(layer, previous) * np.sqrt(2 / previous))
+            previous = layer
+
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = weights
+
+    @property
+    def L(self):
+        """
+        gets the private instance attribute __L
+        """
+        return self.__L
+
+    @property
+    def cache(self):
+        """
+        gets the private instance attribute __cache
+        """
+        return self.__cache
+
+    @property
+    def weights(self):
+        """
+        gets the private instance attribute __weights
+        """
+        return self.__weights
+
+    def forward_propagation(self, X):
+        """
+        Performs the forward propagation for the deep neural network
+        """
+        self.cache["A0"] = X
+        for i in range(1, self.L + 1):
+            A_prev = self.cache["A{}".format(i - 1)]
+            W = self.weights["W{}".format(i)]
+            b = self.weights["b{}".format(i)]
+            Z = np.dot(W, A_prev) + b
+            self.cache["A{}".format(i)] = 1 / (1 + np.exp(-Z))
+
+        return self.cache["A{}".format(self.L)], self.cache
