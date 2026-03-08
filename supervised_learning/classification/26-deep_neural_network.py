1dd01b9af39860f1373618ce8d01cf1747a48468
farizakb <farizakbarzada@gmail.com>
2026-03-08 18:15:05 +0400

    commit 26

---
+++ supervised_learning/classification/26-deep_neural_network.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python3
+"""
+DeepNeuralNetwork performing binary classification
+"""
+
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """
+    Class that represents a deep neural network for binary classification
+    """
+
+    def __init__(self, nx, layers):
+        """
+        Class constructor
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(layers) is not list or len(layers) < 1:
+            raise TypeError("layers must be a list of positive integers")
+
+        weights = {}
+        previous = nx
+
+        for index, layer in enumerate(layers, 1):
+            if type(layer) is not int or layer < 0:
+                raise TypeError("layers must be a list of positive integers")
+
+            weights["b{}".format(index)] = np.zeros((layer, 1))
+            weights["W{}".format(index)] = (
+                np.random.randn(layer, previous) * np.sqrt(2 / previous))
+            previous = layer
+
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = weights
+
+    @property
+    def L(self):
+        """
+        gets the private instance attribute __L
+        """
+        return self.__L
+
+    @property
+    def cache(self):
+        """
+        gets the private instance attribute __cache
+        """
+        return self.__cache
+
+    @property
+    def weights(self):
+        """
+        gets the private instance attribute __weights
+        """
+        return self.__weights
+
+    def forward_prop(self, X):
+        """
+        Performs the forward propagation for the deep neural network
+        """
+        self.__cache["A0"] = X
+        for i in range(1, self.L + 1):
+            A_prev = self.__cache["A{}".format(i - 1)]
+            W = self.weights["W{}".format(i)]
+            b = self.weights["b{}".format(i)]
+            Z = np.dot(W, A_prev) + b
+            A = 1 / (1 + np.exp(-Z))
+            self.__cache["A{}".format(i)] = A
+
+        return A, self.__cache
+
+    def cost(self, Y, A):
+        """
+        Calculates the cost of the neural network
+        """
+        m = Y.shape[1]
+        cost = -1 / m * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        return np.squeeze(cost)
+
+    def evaluate(self, X, Y):
+        """
+        Evaluates the neural network
+        """
+        A, _ = self.forward_prop(X)
+        cost = self.cost(Y, A)
+        pred = np.where(A >= 0.5, 1, 0)
+        return pred, cost
+
+    def gradient_descent(self, Y, cache, alpha=0.05):
+        """
+        Performs the gradient descent for the deep neural network
+        """
+        m = Y.shape[1]
+
+        # derivative of final layer (output=self.L)
+        dZ = cache["A{}".format(self.L)] - Y
+
+        for i in range(self.L, 0, -1):
+            A_prev = cache["A{}".format(i-1)]
+            dW = (1/m) * np.dot(dZ, A_prev.T)
+            db = (1/m) * np.sum(dZ, axis=1, keepdims=True)
+            dZ = np.dot(self.weights["W{}".format(i)].T, dZ) * (
+                A_prev * (1 - A_prev))
+
+            self.weights["W{}".format(i)] -= alpha * dW
+            self.weights["b{}".format(i)] -= alpha * db
+
+    def train(self, X, Y, iterations=5000, alpha=0.05):
+        """
+        Trains the neural network
+        """
+        if not isinstance(iterations, int):
+            raise TypeError("iterations must be an integer")
+        if iterations < 0:
+            raise ValueError("iterations must be a positive integer")
+        if not isinstance(alpha, float):
+            raise TypeError("alpha must be a float")
+        if alpha < 0:
+            raise ValueError("alpha must be positive")
+
+        for i in range(iterations):
+            self.forward_prop(X)
+            self.gradient_descent(Y, self.cache, alpha)
+
+        return self.evaluate(X, Y)
+
+
+    def save(self, filename):
+        """
+            Method to saves instance object to a file in pickle format
+
+            :param filename: file which the object should be saved
+
+        """
+        # test extention
+        if not filename.endswith('.pkl'):
+            filename += '.pkl'
+
+        # open file in binary write mode
+        with open(filename, 'wb') as file:
+            # use pickel to dump the object into the file
+            pickle.dump(self, file)
+
+    @staticmethod
+    def load(filename):
+        """
+            method to load a pickled DeepNeuralNetwork object
+
+            :param filename: file from which object should be loaded
+
+            :return: loaded object
+                    or None if filename doesn't exist
+        """
+
+        try:
+            # open file in binary mode
+            with open(filename, 'rb') as file:
+                # use pickle to load
+                loaded_object = pickle.load(file)
+            return loaded_object
+
+        except FileNotFoundError:
+            return None
