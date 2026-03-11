72151f7e98f78696b30897344a9af3cd5faf593b
farizakb <farizakbarzada@gmail.com>
2026-03-11 16:07:49 +0400

    commit 8

--- supervised_learning/keras/8-train.py
+++ supervised_learning/keras/8-train.py
@@ -57,7 +57,7 @@ def train_model(network, data, labels, batch_size, epochs,
 
     history = network.fit(x=data,
                           y=labels,
-                          epochs=epochs,    
+                          epochs=epochs,
                           batch_size=batch_size,
                           validation_data=validation_data,
                           callbacks=callbacks,
