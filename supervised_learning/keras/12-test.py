84aaa6d77e59b72a70a34c572ef28b9c4a0b404f
farizakb <farizakbarzada@gmail.com>
2026-03-11 17:42:14 +0400

    commit 12

---
+++ supervised_learning/keras/12-test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+    Test model function
+"""
+
+import tensorflow.keras as K
+
+
+def test_model(network, data, labels, verbose=True):
+    """
+        function that tests a model
+
+        :param network: model to test
+        :param data: ndarray, shape(m, nx), input data
+        :param labels: ndarray, shape(m,classes), labels
+        :param verbose: boolean, print or not during testing
+
+        :return: None
+    """
+    return network.evaluate(x=data, y=labels, verbose=verbose)
