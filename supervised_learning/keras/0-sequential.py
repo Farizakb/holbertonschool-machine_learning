810815d04fe639c2b14182504c19fc00b7170a62
farizakb <farizakbarzada@gmail.com>
2026-03-11 14:30:23 +0400

    commit 0.1

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -15,9 +15,14 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     keep_prob: dropout rate
     """
     model = K.Sequential()
-    model.add(K.layers.Dense(layers[0], input_dim=nx, activation=activations[0], kernel_regularizer=K.regularizers.l2(lambtha)))
-    for i in range(1, len(layers)):
-        model.add(K.layers.Dense(layers[i], activation=activations[i], kernel_regularizer=K.regularizers.l2(lambtha)))
+
+    for i in range(len(layers)):
+        model.add(K.layers.Dense(layers[i],
+                  activation=activations[i],
+                  kernel_regularizer=K.regularizers.L2(lambtha),
+                  input_dim=nx))
+        # apply dropout except on output layer
         if i != len(layers) - 1 and keep_prob is not None:
             model.add(K.layers.Dropout(1-keep_prob))
+
     return model
