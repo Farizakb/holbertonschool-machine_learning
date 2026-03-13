4150083d25beac122a01ed3ec0aef03eab8f158c
farizakb <farizakbarzada@gmail.com>
2026-03-13 15:04:23 +0400

    commit 12

---
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+    Learning Rate Decay TensorFlow
+"""
+
+import tensorflow as tf
+
+
+def learning_rate_decay(alpha, decay_rate, decay_step):
+    """
+    Creates a TensorFlow optimization operation for learning rate decay.
+
+    :param alpha: float, initial learning rate
+    :param decay_rate: float, decay rate
+    :param decay_step: int, number of iterations after which to decay the learning rate
+    :return: tf.keras.optimizers.schedules.ExponentialDecay instance
+    """
+    return tf.keras.optimizers.schedules.ExponentialDecay(
+        initial_learning_rate=alpha,
+        decay_steps=decay_step,
+        decay_rate=decay_rate,
+        staircase=True
+    )
