bba7c3f9bd470dc62b455f3e6b0138c4328fa086
farizakb <farizakbarzada@gmail.com>
2026-03-13 14:50:07 +0400

    commit 9

---
+++ supervised_learning/optimization/9-Adam.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+    Adam Optimization
+"""
+
+import numpy as np
+
+
+def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
+    """
+    Update variables using the Adam optimization algorithm.
+
+    :param alpha: float, learning rate
+    :param beta1: float, decay rate for the moving average of first moments
+    :param beta2: float, decay rate for the moving average of second moments
+    :param epsilon: float, small constant to prevent division by zero
+    :param var: ndarray, current variable values
+    :param grad: ndarray, gradient of the cost function with respect to var
+    :param v: ndarray, moving average of first moments
+    :param s: ndarray, moving average of second moments
+    :param t: int, iteration number
+    :return: tuple containing updated variables (var, v, s)
+    """
+    # Update moving average of first moments
+    v = beta1 * v + (1 - beta1) * grad
+
+    # Update moving average of second moments
+    s = beta2 * s + (1 - beta2) * (grad ** 2)
+
+    # Update variables
+    var = var - alpha * v / (np.sqrt(s) + epsilon)
+
+    return var, v, s
