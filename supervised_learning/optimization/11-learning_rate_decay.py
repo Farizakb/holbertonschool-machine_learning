9fd18f6d432719b5d998ea12609b767f637f4d72
farizakb <farizakbarzada@gmail.com>
2026-03-13 14:59:30 +0400

    commit 11

---
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+    Learning Rate Decay
+"""
+
+import numpy as np
+
+
+def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """
+    Calculates the learning rate after a decay schedule.
+
+    :param alpha: float, initial learning rate
+    :param decay_rate: float, decay rate
+    :param global_step: int, current iteration number
+    :param decay_step: int, number of iterations after which to decay the learning rate
+    :return: float, new learning rate
+    """
+    return alpha * (1 / (1 + decay_rate * global_step / decay_step))
